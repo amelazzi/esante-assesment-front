f59f9936a2578736239c342d2e49515285fe9aff
AAZ <amelias.azzi@gmail.com>
2025-08-12 18:01:26 +0200

    ✨ Route posts page

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,17 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { RoutePaths } from "./RoutePaths";
+import { Posts } from "../pages/posts/Posts";
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route
+          path={RoutePaths.HOME}
+          element={<Navigate to={RoutePaths.POSTS} replace />}
+        />
+        <Route path={RoutePaths.POSTS} element={<Posts />} />
+      </Routes>
     </BrowserRouter>
   );
 };
