ca86b7c832192352f2fdf9ccbffd85942a695e4f
AAZ <amelias.azzi@gmail.com>
2025-08-12 17:47:39 +0200

    🎉 Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen p-8">
+      <h1 className="text-blue-600 text-4xl font-bold flex justify-center">
+        Welcome to eSante assesment
+      </h1>
     </div>
   );
 }
