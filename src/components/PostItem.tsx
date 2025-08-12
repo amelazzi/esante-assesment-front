596e1bc2f5d0e6894e1c626dfadcffce147939ee
AAZ <amelias.azzi@gmail.com>
2025-08-12 19:02:42 +0200

    ✨ Display posts

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,10 @@
+import { Post } from "../interfaces/Post";
+
+export const PostItem = ({ post }: { post: Post }) => {
+  return (
+    <li className="bg-white rounded-md shadow-md p-6 hover:shadow-lg transition cursor-pointer">
+      <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+      <p className="text-gray-700">{post.body}</p>
+    </li>
+  );
+};
--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,3 +1,21 @@
+import { usePosts } from "../../hooks/usePosts";
+import { ErrorPanel } from "../../components/ErrorPanel";
+import { Loader } from "../../components/Loader";
+import { PostItem } from "../../components/PostItem";
+
 export const Posts = () => {
-  return <h1> posts page</h1>;
+  const { posts, loading, error } = usePosts();
+
+  if (error) return <ErrorPanel />;
+  if (loading) return <Loader />;
+
+  return (
+    <div className="h-screen max-w-3xl mx-auto p-4 flex flex-col">
+      <ul className="flex-1 overflow-y-auto space-y-4">
+        {posts.map((post) => (
+          <PostItem key={post.id} post={post} />
+        ))}
+      </ul>
+    </div>
+  );
 };
